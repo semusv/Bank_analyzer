927f4d57d84c185633ec07bb920266d3e4e99fb1
Semenov V <semvasek@mail.ru>
2025-11-13 10:44:40 +0300

    убрал сеть в тестах

--- backend/src/test/java/ru/vvsem/bank/analyzer/repositories/BaseRepositoryTest.java
+++ backend/src/test/java/ru/vvsem/bank/analyzer/repositories/BaseRepositoryTest.java
@@ -26,8 +26,6 @@ public class BaseRepositoryTest {
                 .withDatabaseName("bank_analyzer_test")
                 .withUsername("test")
                 .withPassword("test")
-//                .withNetworkMode("bank_bbb_bank_analyzer-network")
-//                .withNetworkAliases("testcontainers-db")
                 .withReuse(true);
 
         postgres.start();
